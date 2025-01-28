aa8057b443298c8bf34a48b807953be51ce27382
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-28 20:57:37 -0300

    feat: add queries to filters products

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -15,15 +15,42 @@ const req = axios.create({
   baseURL: "https://api-prova-frontend.solucoeslifeapps.com.br",
 });
 
+export const getProduct = async (id: number): Promise<Product> => {
+  const result = await req.get(`/posts/${id}`);
+  return result.data;
+};
+
 export const getProducts = async (
   page: number = 1,
-  limit: number = 6
+  limit: number = 8
 ): Promise<ProductResponse> => {
   const result = await req.get(`/products?_page=${page}&_per_page=${limit}`);
   return result.data;
 };
 
-export const getProduct = async (id: number): Promise<Product> => {
-  const result = await req.get(`/posts/${id}`);
+export const getProductsByPrice = async (
+  sort: 0 | 1 = 0,
+  page: number = 1,
+  limit: number = 8
+): Promise<ProductResponse> => {
+  const result = await req.get(
+    `/products?_page=${page}&_per_page=${limit}&_sort=${
+      sort === 0 ? "price" : "-price"
+    }`
+  );
+  return result.data;
+};
+
+export const getProductsByCategory = async (
+  category: string,
+  sort: 0 | 1 | null = null,
+  page: number = 1,
+  limit: number = 8
+): Promise<ProductResponse> => {
+  const result = await req.get(
+    `/products?category=${category}&_page=${page}&_per_page=${limit}${
+      sort !== null ? (sort === 0 ? "&_sort=price" : "&_sort=-price") : ""
+    }`
+  );
   return result.data;
 };
