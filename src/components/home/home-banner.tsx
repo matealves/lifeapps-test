3c145207234c792d46b645c9007bfc65c7ddac43
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-27 22:07:53 -0300

    feat: preview banner

---
+++ src/components/home/home-banner.tsx
@@ -0,0 +1,9 @@
+export const HomeBanner = () => {
+  return (
+    <div className="w-full h-96 flex justify-center items-center">
+      <div className="bg-slate-200 w-full max-w-screen-lg h-full flex justify-center items-center">
+        BANNER PROMOÇÃO
+      </div>
+    </div>
+  );
+};
