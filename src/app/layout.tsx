f545742670b5091a5b3f979805446c3072fcc243
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-28 14:33:56 -0300

    fix: title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Ecommerce",
+  title: "E-commerce",
   icons: {
     icon: "/logo.png",
   },
