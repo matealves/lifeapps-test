35511149362e91e6274085fb0115aa0eb4e85c66
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-28 14:34:14 -0300

    feat: new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
-import { redirect } from "next/navigation";
+// import { redirect } from "next/navigation";
 import { HomeHeader } from "@/components/home/home-header";
 import { HomeBanner } from "@/components/home/home-banner";
+import { HomeFilter } from "@/components/home/home-filter";
+import { HomeProducts } from "@/components/home/home-products";
 
 export default function Page() {
   // redirect("/home");
@@ -9,6 +11,8 @@ export default function Page() {
     <div>
       <HomeHeader />
       <HomeBanner />
+      <HomeFilter />
+      <HomeProducts />
     </div>
   );
 }
