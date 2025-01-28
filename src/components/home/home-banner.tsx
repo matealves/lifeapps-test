024be78f0847e046abd867b6c28c59d49c3e8e5e
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-28 20:57:54 -0300

    fix: height banner

--- src/components/home/home-banner.tsx
+++ src/components/home/home-banner.tsx
@@ -1,6 +1,6 @@
 export const HomeBanner = () => {
   return (
-    <div className="w-full h-96 flex justify-center items-center">
+    <div className="w-full h-[450px] flex justify-center items-center">
       <div className="bg-slate-200 w-full max-w-screen-2xl h-full flex justify-center items-center">
         BANNER PROMOÇÃO
       </div>
