e7bcc6eba140bcc135ecc520f572279c2bc8ccd8
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-28 20:58:17 -0300

    fix: style filter

--- src/components/home/home-filter.tsx
+++ src/components/home/home-filter.tsx
@@ -3,15 +3,16 @@ import { SelectOrderBy } from "../ui/select-order-by";
 
 export const HomeFilter = () => {
   return (
-    <div className="w-full h-12 md:h-24 lg:h-24 flex justify-center items-center md:border lg:border border-gray-100">
-      <div className="flex justify-end md:justify-between lg:md:justify-between w-full max-w-screen-2xl mx-10">
+    <div className="flex flex-col w-full h-12 md:h-24 lg:h-24 justify-center items-center mt-10">
+      <div className="flex justify-end md:justify-center lg:md:justify-center w-full max-w-screen-2xl mx-10">
         <div className="hidden md:flex lg:flex gap-2 ">
           <FilterItem label="Todos os Produtos" active />
           <FilterItem label="Camisetas" />
           <FilterItem label="Calças" />
           <FilterItem label="Tênis" />
         </div>
-
+      </div>
+      <div className="flex w-full max-w-screen-2x justify-end mt-4 mr-24">
         <SelectOrderBy />
       </div>
     </div>
