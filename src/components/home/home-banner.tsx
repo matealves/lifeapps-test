80cebd58fde91d880e20a1eb16e4f9fc68dc6864
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-29 14:18:37 -0300

    feat: lib swiper

--- src/components/home/home-banner.tsx
+++ src/components/home/home-banner.tsx
@@ -1,9 +1,41 @@
+"use client";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import Image from "next/image";
+
 export const HomeBanner = () => {
   return (
     <div className="w-full h-[450px] flex justify-center items-center">
-      <div className="bg-slate-200 w-full max-w-screen-2xl h-full flex justify-center items-center">
-        BANNER PROMOÇÃO
-      </div>
+      <Swiper
+        modules={[Autoplay, Pagination]}
+        slidesPerView={1}
+        loop={true}
+        autoplay={{ delay: 7000, disableOnInteraction: false }}
+        pagination={{ clickable: true }}
+        className="w-full max-w-screen-2xl h-full"
+      >
+        <SwiperSlide>
+          <div className="w-full h-full flex justify-center items-center">
+            <img
+              src="/banner1.png"
+              alt="banner1.png"
+              className="w-full bg-cover"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="w-full h-full flex justify-center items-center">
+            <img
+              src="/banner2.png"
+              alt="banner2.png"
+              className="w-full bg-cover"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 };
