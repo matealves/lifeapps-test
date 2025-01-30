800be2da177742851d073d9497ea67768bb0563c
Mateus Alves <contatomateusalves@hotmail.com>
2025-01-30 12:49:10 -0300

    fix: added product to cart

--- src/components/ui/product-individual.tsx
+++ src/components/ui/product-individual.tsx
@@ -1,44 +1,57 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHeart } from "@fortawesome/free-regular-svg-icons";
+import { Product } from "@/types/product";
+import { useState } from "react";
+import { faCheckCircle } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
 
 type Props = {
-  id: string;
-  image: string;
-  name: string;
-  description: string;
-  price: number;
-  discount_percentage: number | boolean;
-  promotional_price: number | boolean;
+  product: Product;
 };
 
-export const ProductIndividual = ({
-  id,
-  image,
-  name,
-  description,
-  price,
-  discount_percentage,
-  promotional_price,
-}: Props) => {
-  const handleClickAddToCart = () => {};
+export const ProductIndividual = ({ product }: Props) => {
+  const [added, setAdded] = useState(false);
+
+  const handleClickAddToCart = (item: Product) => {
+    const getCart = localStorage.getItem("cart_shopping");
+    let cart: Product[] = getCart ? JSON.parse(getCart) : [];
+
+    const itemIndex = cart.findIndex((obj: Product) => obj.id === item.id);
+
+    if (itemIndex !== -1) {
+      cart[itemIndex].qtd = (cart[itemIndex].qtd || 1) + 1;
+    } else {
+      cart.push({ ...item, qtd: 1 });
+    }
+
+    localStorage.setItem("cart_shopping", JSON.stringify(cart));
+
+    // Disparando o evento para atualizar o ícone do carrinho
+    window.dispatchEvent(new Event("cartUpdated"));
+    setAdded(true);
+  };
 
   return (
     <div className="flex mt-20 justify-center min-h-[80vh] w-full">
       <div className="w-full max-w-screen-2xl flex justify-center flex-col md:flex-row lg:flex-row items-center md:items-start lg:items-start">
         <div className="relative h-96 w-96 flex justify-center items-center">
-          <img src={image} className="h-full w-full object-cover" alt={name} />
+          <img
+            src={product.image}
+            className="h-full w-full object-cover"
+            alt={product.name}
+          />
 
-          {discount_percentage && (
+          {product.discount_percentage && (
             <div className="absolute top-2 left-2 bg-red-500 text-white px-4 py-2 text-sm font-bold">
-              {discount_percentage}% OFF
+              {product.discount_percentage}% OFF
             </div>
           )}
         </div>
 
-        <div className="w-96 mb-24 md:mb-0 lg:mb-0 md:ml-28 lg:ml-36 pt-8 flex flex-col justify-between h-96">
+        <div className="w-96 mb-24 md:mb-0 lg:mb-0 md:ml-20 lg:ml-36 pt-8 flex flex-col justify-between h-96">
           <div className="">
             <div className="flex justify-between w-full">
-              <p className="font-bold text-xl mb-2">{name}</p>
+              <p className="font-bold text-xl mb-2">{product.name}</p>
               <FontAwesomeIcon
                 icon={faHeart}
                 className="size-6 text-gray-400 cursor-pointer"
@@ -47,20 +60,20 @@ export const ProductIndividual = ({
 
             <div
               className={`${
-                promotional_price
+                product.promotional_price
                   ? "line-through text-sm opacity-70"
                   : "text-2xl font-bold text-orange-500"
               }`}
             >
-              {price.toLocaleString("pt-br", {
+              {product.price.toLocaleString("pt-br", {
                 style: "currency",
                 currency: "BRL",
               })}
             </div>
 
-            {promotional_price && (
+            {product.promotional_price && (
               <div className="text-2xl font-bold text-orange-500">
-                {promotional_price.toLocaleString("pt-br", {
+                {product.promotional_price.toLocaleString("pt-br", {
                   style: "currency",
                   currency: "BRL",
                 })}
@@ -68,14 +81,32 @@ export const ProductIndividual = ({
             )}
 
             <p className="mt-6 mb-2 text-lg font-semibold">Descrição</p>
-            <p className="text-sm text-gray-500">{description}</p>
+            <p className="text-sm text-gray-500">{product.description}</p>
+
+            <Link href="/cart">
+              <p className="mt-6 text-sm text-orange-500 font-bold text-end">
+                Ir para o carrinho →
+              </p>
+            </Link>
           </div>
 
           <button
-            onClick={handleClickAddToCart}
-            className="uppercase bg-orange-500 hover:bg-orange-400 text-white py-3 rounded-md"
+            onClick={() => handleClickAddToCart(product)}
+            className={`${
+              added ? "bg-blue-500" : "bg-orange-500 hover:bg-orange-400"
+            } uppercase  text-white py-3 rounded-md flex justify-center items-center`}
+            disabled={added ?? false}
           >
-            Adicionar ao carrinho
+            {!added && <span className="">Adicionar ao carrinho</span>}
+            {added && <span className="">Adicionado no carrinho</span>}
+            <span className="">
+              {added && (
+                <FontAwesomeIcon
+                  icon={faCheckCircle}
+                  className="size-4 text-white ml-2"
+                />
+              )}
+            </span>
           </button>
         </div>
       </div>
